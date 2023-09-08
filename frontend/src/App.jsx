55a43c5f77cfbe9ceadf40c46553c5927f3694da
Julio Sevillano <acirdeveloper@gmail.com>
2023-09-07 19:14:10 -0500

    app change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,49 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {useState, useEffect} from "react";
 import ProtectedRoute from "./hooks/ProtectedRoute.jsx";
 // Pages
 import * as Pages from "./pages";
 
 function App() {
-  const [auth, setAuth] = useState(true);
+    const [auth, setAuth] = useState(true);
 
-  useEffect(() => {
-    auth ? localStorage.setItem("auth", "true") : localStorage.removeItem("auth");
-  }, [auth]);
+    useEffect(() => {
+        auth ? localStorage.setItem("auth", "true") : localStorage.removeItem("auth");
+    }, [auth]);
 
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {/* Rutas sin proteger  */}
-            <Route path="/" element={<Pages.Landing/>}/>
-          <Route
-            path="/login"
-            element={<Pages.Login auth={auth} setAuth={setAuth}/>}
-          />
-            <Route path="/register" element={<Pages.Register/>}/>
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    {/* Rutas sin proteger  */}
+                    <Route path="/" element={<Pages.Landing/>}/>
+                    <Route
+                        path="/login"
+                        element={<Pages.Login auth={auth} setAuth={setAuth}/>}
+                    />
+                    <Route path="/register" element={<Pages.Register/>}/>
 
-          {/* Rutas protegidas */}
-          <Route element={<ProtectedRoute auth={auth} />}>
-            <Route
-              path="/dashboard"
-              element={<Pages.Layout/>}
+                    {/* Rutas protegidas */}
+                    <Route element={<ProtectedRoute auth={auth}/>}>
+                        <Route
+                            path="/dashboard"
+                            element={<Pages.Layout/>}
 
-              children={[
-                  <Route key="dashboard" index={true} element={<Pages.Dashboard/>}/>,
-                  <Route key="ingresos" path={"/dashboard/ingresos"} element={<Pages.Ingresos/>}/>,
-                  <Route key="egresos" path={"/dashboard/egresos"} element={<Pages.Egresos/>}/>,
-                  <Route key="inventario" path={"/dashboard/inventario"} element={<Pages.Inventario/>}/>,
-                  <Route key="productos" path={"/dashboard/productos"} element={<Pages.Productos/>}/>,
-                  <Route key="pedidos" path={"/dashboard/pedidos"} element={<Pages.Pedidos/>}/>,
-                  <Route key="settings" path={"/dashboard/settings"} element={<Pages.Setting/>}/>,
-              ]}
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+                            children={[
+                                <Route key="dashboard" index={true} element={<Pages.Dashboard/>}/>,
+                                <Route key="ingresos" path={"/dashboard/ingresos"} element={<Pages.Ingresos/>}/>,
+                                <Route key="egresos" path={"/dashboard/egresos"} element={<Pages.Egresos/>}/>,
+                                <Route key="inventario" path={"/dashboard/inventario"} element={<Pages.Inventario/>}/>,
+                                <Route key="productos" path={"/dashboard/productos"} element={<Pages.Productos/>}/>,
+                                <Route key="pedidos" path={"/dashboard/pedidos"} element={<Pages.Pedidos/>}/>,
+                                <Route key="settings" path={"/dashboard/settings"} element={<Pages.Setting/>}/>,
+                            ]}
+                        />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- frontend/src/components/spinner/SpinnerLoad.jsx
+++ frontend/src/components/spinner/SpinnerLoad.jsx
@@ -2,13 +2,12 @@ import '../css/spinner.css'
 
 function SpinnerLoad() {
   return (
-    
-    <div className="sk-folding-cube">
-  <div className="sk-cube1 sk-cube"></div>
-  <div className="sk-cube2 sk-cube"></div>
-  <div className="sk-cube4 sk-cube"></div>
-  <div className="sk-cube3 sk-cube"></div>
-</div>
+      <div className="sk-folding-cube">
+        <div className="sk-cube1 sk-cube"></div>
+        <div className="sk-cube2 sk-cube"></div>
+        <div className="sk-cube4 sk-cube"></div>
+        <div className="sk-cube3 sk-cube"></div>
+      </div>
   )
 }
 
