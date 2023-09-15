557b15b8abbf35ec4366b718b45ba96cce58c808
Julio Sevillano <acirdeveloper@gmail.com>
2023-09-15 17:30:12 -0500

    nuevo

--- frontend/src/pages/Dashboard/views/Ingresos.jsx
+++ frontend/src/pages/Dashboard/views/Ingresos.jsx
@@ -3,11 +3,12 @@ import CardValues from '../../../components/dashboard/CardValues.jsx'
 import {currentDate, months} from '../../../config/models/ArraysItems.js'
 import ViewMonth from '../../../components/dashboard/ViewMonth.jsx'
 import TableI from '../../../components/Tables/TableI.jsx'
-import { useState } from 'react';
+import {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import useAlert from '../../../hooks/useAlert.jsx'
+import {Card, Container} from "react-bootstrap";
 
 function Ingresos() {
 
@@ -18,107 +19,90 @@ function Ingresos() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const falsaCarga = ()=>{
+    const falsaCarga = () => {
 
         setTimeout(() => {
 
 
             useAlert({
-                type: "success",
-                title: "Cargado con exito",
-                text: '',
-            }   )
+                type: "success", title: "Cargado con exito", text: '',
+            })
             handleClose()
-            
+
         }, 1500);
 
     }
 
 
-    const provedores = ["Proveedor 1" ,
-    "Proveedor 2" ,
-    "Proveedor 3" ,
-    "Proveedor 4" ,
-    "Proveedor 5" ,
-    "Proveedor 6" ,
-    "Proveedor 7" ,
-    "Proveedor 8" ,
-    "Proveedor 9" ,
-
-
-    ]
+    const provedores = ["Proveedor 1", "Proveedor 2", "Proveedor 3", "Proveedor 4", "Proveedor 5", "Proveedor 6", "Proveedor 7", "Proveedor 8", "Proveedor 9"]
 
     return (
+        <Container>
+            <Card>
+                <Card.Header>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Comprar Producto</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
 
-<>
+                                    <Form.Select aria-label="Default select example">
+                                        <option>--Seleccione proveedor--</option>
 
-<>
-     
+                                        {provedores ? provedores.map((prov, key) => {
+                                            return (<option key={key} value={prov}>
+                                                {prov}{" "}
+                                            </option>);
+                                        }) : ""}
+                                    </Form.Select>
+                                    <Form.Label className='mt-4'>Nombre Producto</Form.Label>
+                                    <Form.Control
+                                        type="text"
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Comprar Producto</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        autoFocus
+                                    />
 
-            <Form.Select aria-label="Default select example">
-      <option>--Seleccione proveedor--</option>
+                                    <Form.Label className='mt-4'>Cantidad</Form.Label>
+                                    <Form.Control
+                                        type="number"
 
-      {provedores
-        ? provedores.map((prov, key) => {
-            return (
-              <option key={key} value={prov}>
-                {prov}{" "}
-              </option>
-            );
-          })
-        : ""}
-    </Form.Select> 
-              <Form.Label  className='mt-4'>Nombre Producto</Form.Label>
-              <Form.Control
-                type="text"
-              
-                autoFocus
-              />
-             
-              <Form.Label  className='mt-4' >Cantidad</Form.Label>
-              <Form.Control
-                type="number"
-               
-                autoFocus
-              />
-              <Form.Label  className='mt-4' >Marca</Form.Label>
-              <Form.Control
-                type="text"
-               
-                autoFocus
-              />
-            </Form.Group>
-             
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={handleClose}>
-           Cancelar
-          </Button>
-          <Button variant="primary" onClick={falsaCarga}>
-            Comprar
-          </Button>
-        </Modal.Footer>
-      </Modal>
+                                        autoFocus
+                                    />
+                                    <Form.Label className='mt-4'>Marca</Form.Label>
+                                    <Form.Control
+                                        type="text"
 
-    </>
-        <div>
-            <h1 className='fs-4 mb-2 text-gray-500'>Ingresos</h1>
-            {ViewMonth(month, months)}
-            <Button variant="primary" className='mt-3 ' onClick={handleShow}>
-      Comprar Productos
-      </Button>
-            <TableI/>
-        </div>
-        </>
+                                        autoFocus
+                                    />
+                                </Form.Group>
+
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="danger" onClick={handleClose}>
+                                Cancelar
+                            </Button>
+                            <Button variant="primary" onClick={falsaCarga}>
+                                Comprar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </Card.Header>
+                <hr/>
+                <Card.Body>
+                    <div>
+                        <h1 className='fs-4 mb-2 text-gray-500'>Ingresos</h1>
+                        {ViewMonth(month, months)}
+                        <Button variant="primary" className='mt-3 ' onClick={handleShow}>
+                            Comprar Productos
+                        </Button>
+                        <TableI/>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
 
